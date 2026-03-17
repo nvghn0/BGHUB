3558e79a30a8061fa114e3b927609d4e68cf5a13
nvghn0 <gopalbharvad040@gmail.com>
2026-03-17 22:21:12 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,6 @@ import Login from "./pages/Login";
 import Addresses from "./pages/Addresses";
 
 import AdminProtectedRoute from "./components/AdminProtectedRoute";
-
 import AdminLogin from "./admin/AdminLogin";
 import AdminDashboard from "./admin/AdminDashboard";
 import AdminGrocery from "./admin/AdminGrocery";
@@ -17,9 +16,7 @@ import AdminProducts from "./admin/AdminProducts";
 
 function App() {
   return (
-
     <Routes>
-
       <Route path="/" element={<Home />} />
       <Route path="/grocery" element={<Grocery />} />
       <Route path="/cart" element={<Cart />} />
@@ -28,45 +25,11 @@ function App() {
       <Route path="/addresses" element={<Addresses />} />
 
       <Route path="/P5K4B7" element={<AdminLogin />} />
-
-      <Route
-        path="/P5K4B7/dashboard"
-        element={
-          <AdminProtectedRoute>
-            <AdminDashboard />
-          </AdminProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/P5K4B7/grocery"
-        element={
-          <AdminProtectedRoute>
-            <AdminGrocery />
-          </AdminProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/P5K4B7/orders"
-        element={
-          <AdminProtectedRoute>
-            <AdminOrders />
-          </AdminProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/P5K4B7/products"
-        element={
-          <AdminProtectedRoute>
-            <AdminProducts />
-          </AdminProtectedRoute>
-        }
-      />
-
+      <Route path="/P5K4B7/dashboard" element={<AdminProtectedRoute><AdminDashboard /></AdminProtectedRoute>} />
+      <Route path="/P5K4B7/grocery" element={<AdminProtectedRoute><AdminGrocery /></AdminProtectedRoute>} />
+      <Route path="/P5K4B7/orders" element={<AdminProtectedRoute><AdminOrders /></AdminProtectedRoute>} />
+      <Route path="/P5K4B7/products" element={<AdminProtectedRoute><AdminProducts /></AdminProtectedRoute>} />
     </Routes>
-
   );
 }
 
