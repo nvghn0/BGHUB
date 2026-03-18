cf9b623e78f492ec80651cdecd44ab088b624115
nvghn0 <gopalbharvad040@gmail.com>
2026-03-18 22:42:01 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import AppRoutes from "./routes/AppRoutes";
 import { AuthProvider } from "./context/AuthContext";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <AuthProvider>
+      <Navbar />
       <AppRoutes />
     </AuthProvider>
   );
