ccd744449a55f15c5e33777bf70aa6ece8c448fb
nvghn0 <gopalbharvad040@gmail.com>
2026-03-19 00:07:06 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://curly-computing-machine-r4jrvqwjwgxxfp79-5000.app.github.dev/",
+  baseURL: "https://curly-computing-machine-r4jrvqwjwgxxfp79-5000.app.github.dev/api",
 });
 
 // 🔥 VERY IMPORTANT
