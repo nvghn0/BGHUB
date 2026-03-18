275777fdf950a4e2f40d5e0270e9831bf9a57bcd
nvghn0 <gopalbharvad040@gmail.com>
2026-03-18 21:25:09 +0530

    Update AppRoutes.jsx

--- frontend/src/Routes/AppRoutes.jsx
+++ frontend/src/Routes/AppRoutes.jsx
@@ -1,16 +1,42 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import Home from "../pages/Home";
+import ProtectedRoute from "../components/ProtectedRoute";
 
 const AppRoutes = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        {/* Public Routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Protected User Route */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Admin Route */}
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute adminOnly={true}>
+              <h1>Admin Panel</h1>
+            </ProtectedRoute>
+          }
+        />
+
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default AppRoutes;
